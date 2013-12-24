168baef467337ed94e132fc31fee73d58609579f
arainfor <arainfor@gmail.com>
2013-12-24 12:29:26 -0600

    Run in background with nohup

--- therm/daemon/run.sh
+++ therm/daemon/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp /home/arainfor/therm/daemon/thermronstat.jar:/home/arainfor/therm/daemon com/ronhull/thermronstat/daemon/Main
+nohup java -cp /home/arainfor/therm/daemon/thermronstat.jar:/home/arainfor/therm/daemon com/ronhull/thermronstat/daemon/Main&
